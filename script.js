db535025e66c893707eead0fa7d625d0021e2f93
siddharthpaul2005 <paulsiddharth000@gmail.com>
2026-02-20 00:26:18 +0530

    this is adding the portfolio inside the to_do_app

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let input = document.querySelector("input")
-
+/*
+to access the id write it with the # 
+and to access it with the class then use .
+*/
 let btn = document.querySelector("button")
 
 let list = document.querySelector("ol")
@@ -13,10 +16,17 @@ function taskHandler() {
         alert("Please Enter Task before adding")
         return
     }
+    //creating a new element
     let li = document.createElement("li")
 
     let taskText = document.createElement("span")
     taskText.innerText = data
+    /*
+    two mathods to add data 
+    inner text and inner HTML 
+    in inner text - it will consider the tag H1 it wil consider it as a text
+    in inner text - it will consider the tag H1 it wil consider it as a tag
+    */
 
     let del = document.createElement("button")
     del.innerText = "Delete"
